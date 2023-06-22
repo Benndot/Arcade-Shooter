2bfdc49549fc76eac02fa99ab39c713dbeffba3f
b-kiraly <b.m.kiraly@gmail.com>
2023-06-22 00:46:13 -0400

    added projectiles and movement
    also significantly cleaned up the player movement code. Started a stage
    class that I haven't done much with so far.

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ class Image:
         Screen.screen.blit(self.image, (adjusted_x, 0))
 
 
+backdrop = Image("images/backdrop_park.png", Screen.width * 0.66, Screen.height)
+
+
 class Entity:
 
     def __init__(self, name: str, form: Image, x: float or int, y: float or int):
@@ -48,13 +51,40 @@ class Entity:
         Screen.screen.blit(self.form.image, (self.x, self.y))
 
 
-class Battler(Entity):
+class Player(Entity):
 
     def __init__(self, name, form, x, y, health):
         super().__init__(name, form, x, y)
         self.health = health
         self.momentum = 0
+        self.projectile_image = Image("images/baseball.png", Screen.game_zone / 20, Screen.game_zone / 20)
+        self.projectiles = []
 
+    def move(self):
+        if self.momentum != 0:
+            self.x += self.momentum
+            if self.x <= (Screen.width - backdrop.image.get_width()) / 2:
+                self.x = (Screen.width - backdrop.image.get_width()) / 2
+            if self.x >= ((Screen.width - backdrop.image.get_width()) / 2) + backdrop.width - self.form.width:
+                self.x = ((Screen.width - backdrop.image.get_width()) / 2) + backdrop.width - self.form.width
+        self.display()
+
+    def move_projectiles(self):
+        for projectile in self.projectiles:
+            projectile.y -= Screen.height / 112
+            if projectile.y < -50:
+                self.projectiles.remove(projectile)
+            projectile.display()
+
+
+class Stage:
+
+    def __init__(self, background):
+        self.background: Image = background
+        self.enemy_list: list = []
+
+
+# GAME SCREENS --------------------------------------------------------------------------------------------------------
 
 def start_menu():
 
@@ -81,10 +111,8 @@ def game():
 
     Screen.screen.fill((0, 0, 0))
 
-    backdrop = Image("images/backdrop_park.png", Screen.width * 0.66, Screen.height)
-
     player_image = Image("images/dad.png", Screen.game_zone / 11, Screen.game_zone / 11)
-    player = Battler("Player", player_image, Screen.width / 2, Screen.height - player_image.height, 3)
+    player = Player("Player", player_image, Screen.width / 2, Screen.height - player_image.height, 3)
 
     while True:
 
@@ -95,24 +123,24 @@ def game():
                 pygame.quit()
                 sys.exit()
             if evnt.type == pygame.KEYDOWN:
-                if evnt.key == pygame.K_RIGHT:
+                if evnt.key == pygame.K_RIGHT or evnt.key == pygame.K_d:
                     player.momentum = Screen.height / 112
-                if evnt.key == pygame.K_LEFT:
+                if evnt.key == pygame.K_LEFT or evnt.key == pygame.K_a:
                     player.momentum = - Screen.height / 112
+                if evnt.key == pygame.K_SPACE:
+                    projectile = Entity("Projectile", player.projectile_image, player.x +
+                                        (player.projectile_image.width / 2), player.y - Screen.height / 30)
+                    player.projectiles.append(projectile)
             if evnt.type == pygame.KEYUP:
-                if evnt.key == pygame.K_RIGHT or evnt.key == pygame.K_LEFT:
+                if evnt.key == pygame.K_RIGHT or evnt.key == pygame.K_LEFT or evnt.key == pygame.K_d or \
+                        evnt.key == pygame.K_a :
                     player.momentum = 0
                 if evnt.key == pygame.K_ESCAPE:
                     main()
 
-        if player.momentum != 0:
-            player.x += player.momentum
-            if player.x <= (Screen.width - backdrop.image.get_width()) / 2:
-                player.x = (Screen.width - backdrop.image.get_width()) / 2
-            if player.x >= ((Screen.width - backdrop.image.get_width()) / 2) + backdrop.width - player.form.width:
-                player.x = ((Screen.width - backdrop.image.get_width()) / 2) + backdrop.width - player.form.width
+        player.move()
 
-        player.display()
+        player.move_projectiles()
 
         pygame.display.update()
         clock.tick(Game.fps)
